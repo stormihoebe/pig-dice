f84b66d4fa86c2785b305d8523d3e2a6e50b2cc6
oliver fu and Stormi Hoebelheinrich <stormi@>
2017-03-01 10:01:04 -0800

    Create function for appending array of rolls to list, and function for calculating Turn Score

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
 var dice
-var humanTurnScore
-var humanTotalScore
-var humanRollsArray = []
+var humanTurnScore = 0;
+var humanTotalScore = 0;
+var humanRollsArray = [];
 //Generates random number 1-6
 var reset = function() {
   humanTurnScore = 0;
   humanTotalScore= 0;
   humanRollsArray = [];
-
 };
+
 var diceRoll = function(){
-return dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
+dice = Math.floor(Math.random() * 6) + 1;
+humanRollsArray.push(dice);
+
 };
 
 $(function(){
@@ -20,6 +21,25 @@ $(function(){
     reset();
   });
 
+  $("#roll").click(function(event){
+    diceRoll();
+  //Displays list of rolls for your turn
+    $("ul#humanRolls").text("");
+    humanRollsArray.forEach(function(roll){
+      $("ul#humanRolls").append("<li>" + roll + "</li>");
+    });
+  // $("#humanRolls").text(humanRollsArray);
+    addToTurnScore();
+  });
+
+  var addToTurnScore = function() {
+    if (dice === 1){
+      humanTurnScore = 0
+    } else {
+    humanTurnScore = humanTurnScore + dice
+    $("#humanTurnScore").text(humanTurnScore);
+  };
+  };
 
 
 
