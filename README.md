77202549bce35472c759e96213c6dff93678095e
oliver fu and Stormi Hoebelheinrich <oliver@>
2017-03-01 11:25:12 -0800

    create constructor for two players, add form to collect players names

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 ## pig dice
-* generate random number
+* generate random number /
 * (Be able to switch from computer to user)
-* add dice roll to turn score
-* add exception for number 1, so turn score wwill be 0
+* add dice roll to turn score /
+* add exception for number 1, so turn score wwill be 0 /
 * Create hold option, so that turn score is added to total score, and it becomes other player's turn.
 * computer and user can both hold and roll
+
+* Each Roll/Hold counts as even or odd ? Even or odd determines where data is stored. 
--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,94 @@
+//play constructor
+function Player(name, turnScore, totalScore, turnRollArray) {
+  this.name = name;
+  this.turnScore = turnScore;
+  this.totalScore = totalScore;
+  this.turnRollArray = turnRollArray;
+};
+
+var player1 = new Player(player1, player1TurnScore, player1TotalScore, player1RollsArray);
+var player2 = new Player(player2, player2TurnScore, player2TotalScore, player2RollsArray);
 var dice
+//generic rolls & scores
 var humanTurnScore = 0;
 var humanTotalScore = 0;
 var humanRollsArray = [];
+
+//player1 rolls & scores
+var player1Name;
+var player1TurnScore = 0;
+var player1TotalScore = 0;
+var player1RollsArray = [];
+//player2 rolls & scores
+var player2Name;
+var player2TurnScore = 0;
+var player2TotalScore = 0;
+var player2RollsArray = [];
+
+//a function to toggle between players
+var switchPlayer = function() {
+
+};
+
+
 //Generates random number 1-6
 var reset = function() {
   humanTurnScore = 0;
   humanTotalScore= 0;
   humanRollsArray = [];
 };
-
 var diceRoll = function(){
 dice = Math.floor(Math.random() * 6) + 1;
 humanRollsArray.push(dice);
-
 };
 
 $(function(){
 //reset game to start playing
-  $("#reset").click(function(event){
+  $("#namesForm").submit(function(event){
+    event.preventDefault();
     reset();
-  });
+    player1Name = $("#player1Name").val();
+    player2Name = $("#player2Name").val();
 
+  });
+//Roll Dice button
   $("#roll").click(function(event){
+  //generate random number 1-6 for dice roll
     diceRoll();
   //Displays list of rolls for your turn
-    $("ul#humanRolls").text("");
+    $("ul#player1Rolls").text("");
     humanRollsArray.forEach(function(roll){
-      $("ul#humanRolls").append("<li>" + roll + "</li>");
+      $("ul#player1Rolls").append("<li>" + roll + "</li>");
     });
-  // $("#humanRolls").text(humanRollsArray);
+  // adds dice roll to turn score, sets turn score to zero when you roll a 1, then prints turn score to HumanTurnScore
     addToTurnScore();
   });
+//Hold button
+  $("#hold").click(function(event){
+    endTurn();
+  });
 
+// function to add dice roll to turn score, sets turn score to zero when you roll a 1, then prints turn score to HumanTurnScore
   var addToTurnScore = function() {
     if (dice === 1){
       humanTurnScore = 0
+      endTurn();
     } else {
     humanTurnScore = humanTurnScore + dice
-    $("#humanTurnScore").text(humanTurnScore);
+    $("#player1TurnScore").text(humanTurnScore);
   };
   };
-
+// function for ending the turn, totaling up turn score with total score and printing to screen
+var endTurn = function() {
+  humanTotalScore += humanTurnScore;
+  $("#player1TotalScore").text(humanTotalScore);
+  alert("Your Turn is Over!");
+  humanTurnScore = 0;
+  $("#player1TurnScore").text(humanTurnScore);
+  humanRollsArray = [];
+  $("#player1Rolls").text(humanRollsArray);
+  switchPlayer();
+};
 
 
 
