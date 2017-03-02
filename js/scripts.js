79d4d8ff06be2e56adc2a202a84369a6b2c89a93
oliver fu and Stormi Hoebelheinrich <oliver@>
2017-03-01 16:44:44 -0800

    program is able to store total score and switch user, displaying appropriate total scores

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,35 @@
 // //play constructor
-// function Player(name, turnScore, totalScore, turnRollArray) {
-//   this.name = name;
-//   this.turnScore = turnScore;
-//   this.totalScore = totalScore;
-//   this.turnRollArray = turnRollArray;
-// };
+function Player(name, totalScore) {
+  this.playerName = name;
+  this.totalScore = totalScore;
+};
+
+// var savedScore = new Player();
 //
-// var player1 = new Player();
-// var player2 = new Player();
+var player1;
+var player2;
 
 var dice;
 var playerTurn;
 var playerTurnScore;
+var playerTotalScore;
 var player1TotalScore = 0;
 var player2TotalScore = 0;
 var rollsArray = [];
 var nameP1;
 var nameP2;
-//a function to toggle between players
-var switchPlayer = function() {
-  // $("#player2").toggle();
-  // $("#player1").toggle();
-  if (playerTurn === "player1") {
-    playerTurn = "player2";
-  } else if (playerTurn === "player2") {
-    playerTurn = "player1"
 
-  };
-};
-//Generates random number 1-6
-var reset = function() {
+//resets all game values
+var resetAll = function() {
   playerTurnScore = 0;
+  rollsArray = [];
   player1TotalScore = 0;
   player2TotalScore = 0;
-  rollsArray = [];
 };
-var diceRoll = function(){
-dice = Math.floor(Math.random() * 6) + 1;
-rollsArray.push(dice);
+//resets turn values
+var reset = function() {
+  playerTurnScore = 0;
+  rollsArray = [];
 };
 
 
@@ -45,67 +37,99 @@ $(function(){
 //reset game to start playing
   $("#namesForm").submit(function(event){
     event.preventDefault();
-    reset();
+    resetAll();
     nameP1 = $("#player1Name").val();
     nameP2 = $("#player2Name").val();
-    $("#player1").show();
+    $("#playerColumn").show();
     playerTurn = "player1"
     console.log(playerTurn);
+    player1 = new Player(nameP1, player1TotalScore);
+    player2 = new Player(nameP2, player2TotalScore);
     playerNameFunction();
+    player1TotalScore = 0;
+    player2TotalScore = 0
     if (playerTurn === "player1") {
       gamePlay(player1);
-
     }  else {
       gamePlay(player2);
     };
   });
-
+//a function to toggle between players
+var switchPlayer = function() {
+  if (playerTurn === "player1") {
+    playerTurn = "player2";
+  } else if (playerTurn === "player2") {
+    playerTurn = "player1";
+  };
+};
 var gamePlay = function(player){
-//Roll Dice button
-    $("#roll").click(function(){
-//generate random number 1-6 for dice roll
-      diceRoll();
-      console.log(dice);
+  rollsArray = [];
+  $("#playerRolls").text("");
+  playerTurnScore = 0;
+  $("#turnScore").text(playerTurnScore);
+  if (playerTurn === "player1") {
+    $("#playerTotalScore").text(player1.totalScore);
+  } else if (playerTurn === "player2"){
+    $("#playerTotalScore").text(player2.totalScore);
+  };
+  playerNameFunction();
+};//end gamePlay
+
+//gibes random number 1-6 as dice roll, collects rolls in array, appends array items to page in "Rolls" section ALSO adds dice roll to turn score, sets turn score to zero when you roll a 1, then prints turn score to turnScore
+var diceRoll = function(){
+dice = Math.floor(Math.random() * 6) + 1;
+rollsArray.push(dice);
 //Displays list of rolls for your turn
-      $("ul#playerRolls").text("");
-      rollsArray.forEach(function(roll){
-        $("ul#playerRolls").append("<li>" + roll + "</li>");
-      });
-// adds dice roll to turn score, sets turn score to zero when you roll a 1, then prints turn score to turnScore
-      turnScoreFunction();
-    });
-
+  $("ul#playerRolls").text("");
+  rollsArray.forEach(function(roll){
+    $("ul#playerRolls").append("<li>" + roll + "</li>");
+    turnScoreFunction();
+  });
+};
+//Roll Dice button
+  $("#roll").click(function(){
+    diceRoll();
+  });
 // function to add dice roll to turn score, sets turn score to zero when you roll a 1, then prints turn score to turnScore
-      var turnScoreFunction = function() {
-        if (dice === 1){
-          playerTurnScore = 0
-          $("#turnScore").html(playerTurnScore);
-          endTurn();
-        } else {
-        playerTurnScore += dice;
-        $("#turnScore").html(playerTurnScore);
+  var turnScoreFunction = function() {
+    if (dice === 1){
+      playerTurnScore = 0;
+      $("#turnScore").text(playerTurnScore);
+      endTurn();
+    } else {
+    playerTurnScore += dice;
+    $("#turnScore").text(playerTurnScore);
+  };
+  };
+  $("#hold").click(function(event){
+    endTurn();
+  });
 
+// function for ending the turn, totaling up turn score with total score and printing to screen
+    var endTurn = function() {
+      if (playerTurn === "player1"){
+        player1.totalScore += playerTurnScore;
+        $("#playerTotalScore").text(player1TotalScore);
+      } else {
+        player2.totalScore += playerTurnScore;
+        $("#playerTotalScore").text(player2TotalScore);
       };
-      };
-      // function for ending the turn, totaling up turn score with total score and printing to screen
-      // var endTurn = function() {
-      //   playerTotalScore += playerTurnScore;
-      //   $("#playerTotalScore").text(playerTotalScore);
-      //   alert("Your Turn is Over!");
-      //   playerTurnScore = 0;
-      //   $("#player1TurnScore").text(playerTurnScore);
-      //   playerRollsArray = [];
-      //   $("#playerRolls").text(playerRollsArray);
-      //   switchPlayer();
-      // };
-      // $("#hold").click(function(event){
-      //   endTurn();
-      // });
-};//end gamePlayFunction
+
+      playerTurnScore = 0;
+      $("#turnScore").text(playerTurnScore);
+      RollsArray = [];
+      $("#playerRolls").text(rollsArray);
+      alert("Your Turn is Over!");
+      switchPlayer();
+      gamePlay();
+    };
+
+
+
+
+
 
 //Hold button
-
-
 
 //name assignment function
 var playerNameFunction = function(){
